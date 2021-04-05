6b36791e1f41598dabb49bcfa2964947b488bbb4
andrea_daza <tangananica24@gmail.com>
2021-04-05 18:19:21 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button, Label, DoubleVar, Entry
+
+window = Tk()
+window.title("Convertidor de pies a metros")
+window.configure(background="light green")
+window.geometry("320x220")
+window.resizable(width=False, height=False)
+
+window.mainloop()
